6754ffe16fa0577b0752e1c501dd29485b56d7af
Casey <caseysims07@gmail.com>
2026-09-08 13:02:52 +0100

    Insert, Update, Delete functionality added

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sqlite3
 
+
 #DATABASE SETUP
 #=====================================================================
 conn = sqlite3.connect("finances.db")
 cursor = conn.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS finances")
+# cursor.execute("DROP TABLE IF EXISTS finances")
 
 table_creation_query = """
-    CREATE TABLE finances 
+    CREATE TABLE IF NOT EXISTS finances 
     (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
         NAME TEXT NOT NULL,
         AMOUNT INTEGER NOT NULL,
         CATEGORY TEXT NOT NULL
@@ -18,11 +20,27 @@ table_creation_query = """
 cursor.execute(table_creation_query)
 #===================================================================
 
+def insert_finances(name, amount, category):
+    cursor.execute("INSERT INTO finances  (NAME, AMOUNT, CATEGORY) VALUES (?,?,?)", (name, amount, category))
+    conn.commit()
+
+
+def remove_finances(name):
+    cursor.execute("DELETE FROM finances WHERE NAME = ?", (name,))
+    conn.commit()
+
+def update_finances(id, name, amount, category):
+    cursor.execute("UPDATE finances SET NAME = ?, AMOUNT = ?, CATEGORY = ? WHERE ID = ?", (name, amount, category, id))
+    conn.commit()
 
 
 
 
 
+insert_finances("John", "5000", "RENT")
+
+
+update_finances("1","John", "500", "BILLS")
 
 
 
